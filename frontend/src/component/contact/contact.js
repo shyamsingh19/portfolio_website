fc4ff6775574102d57fe6fe66c861ae2aba35cd5
shyam sunder singh <shyamsunders507@gmail.com>
2024-03-19 00:46:58 +0530

    changed url

--- frontend/src/component/contact/contact.js
+++ frontend/src/component/contact/contact.js
@@ -10,7 +10,8 @@ import git from "../../assets/git.png";
 import github from "../../assets/github.png";
 import emailsent from "../../assets/send.gif";
 import axios from "axios";
-const baseurl = "https://portfolio-backend-psi-two.vercel.app";
+// const baseurl = "https://nodemailer-q1f2.onrender.com";
+const baseurl = "https://portfolio-backend-psi-two.vercel.app/";
 
 const Contact = () => {
   const [name, setName] = useState("");
